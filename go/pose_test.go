8f4a2422cc448e49683731653b89ac159a5ee943
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2021-05-12 22:33:56 +0200

    +Golang Tests

--- go/pose_test.go
+++ go/pose_test.go
@@ -5,6 +5,7 @@ package pose
 
 import (
 	"bufio"
+	"bytes"
 	"io"
 	"os"
 	"testing"
@@ -28,7 +29,42 @@ func testFile(t *testing.T, filename string) {
 	}
 }
 
+func testSampleEqualsOutput(t *testing.T, sample string, expected string) {
+	rbuf := new(bytes.Buffer)
+	_, err1 := rbuf.WriteString(sample)
+	if err1 != nil {
+		t.Error(err1)
+		return
+	}
+
+	rd := bufio.NewReader(rbuf)
+	exps, err := ReadAll(rd)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	buf := new(bytes.Buffer)
+
+	for i, exp := range exps {
+		exp.Write(buf)
+		if i > 0 {
+			io.WriteString(buf, "\n")
+		}
+	}
+	output := buf.String()
+	if output != expected {
+		t.Errorf("output %v not equal to expected %v", output, expected)
+		return
+	}
+}
+
 func TestSRFI(t *testing.T) {
+	testSampleEqualsOutput(t, "(symbol \"value\")", "(symbol \"value\")")
+	testSampleEqualsOutput(t, "; Foo", "")
+	testSampleEqualsOutput(t, "  ; Bar", "")
+	testSampleEqualsOutput(t, "( 1 2  (|asdo\\|aisdj| \"dfdosi dsi\"))", "(1 2 (asdo|aisdj \"dfdosi dsi\"))")
+	testSampleEqualsOutput(t, "()", "()")
+
 	testFile(t, "../examples/hello.pose")
 	testFile(t, "../examples/srfi.pose")
 }
